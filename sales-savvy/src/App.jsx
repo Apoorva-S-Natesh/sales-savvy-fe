fd60fc58ad358ca8192a07158ca44be49cebd3f6
Apoorva-S-Natesh <apoorva1326@gmail.com>
2025-06-05 07:53:48 +0200

    sales-savyy fe auth1

--- sales-savvy/src/App.jsx
+++ sales-savvy/src/App.jsx
@@ -1,6 +1,20 @@
+import { useState } from "react";
+
 function App() {
-	async function send() {
-		const resp = await fetch('http://localhost:8080/data')
+
+	const [username, setUsername] = useState('')
+	
+	function handleChange(e){
+		setUsername(e.target.value)
+	}
+
+	async function handleSubmit(e) {
+		e.preventDefault() //for avoiding automatic submission
+		const resp = await fetch('http://localhost:8080/data',
+			{
+				method: 'POST',
+				body: JSON.stringify({'username': username})
+			});
 		const msg = await resp.text(
 		alert(msg)
 		)
@@ -8,7 +22,14 @@ function App() {
    return (
     <>
       <h4>Welcome to Sales Savvy!</h4>
-	  <button onClick={send}>Click</button>
+	  <form onSubmit={handleSubmit}>
+		<label>Enter username: </label>
+		<input type = "text" name = "username" value = {usrname} 
+		onChange={handleChange}></input>
+		<button type="submit">SEND</button>
+	  </form>
     </>
   )
 }
+
+export default App
