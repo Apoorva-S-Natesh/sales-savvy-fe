8182a8e92cb35b0266ec7140b9d63a75175272c3
Apoorva-S-Natesh <apoorva1326@gmail.com>
2025-06-07 21:49:58 +0200

    user login

--- sales-savvy/src/App.jsx
+++ sales-savvy/src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from "react";
+import Sign_in from "./pages/Sign_in";
+import Sign_up from "./pages/Sign_up";
 
 function App() {
 
-	const [username, setUsername] = useState('')
-	
-	function handleChange(e){
-		setUsername(e.target.value)
-	}
-
-	async function handleSubmit(e) {
-		e.preventDefault() //for avoiding automatic submission
-		const resp = await fetch('http://localhost:8080/data',
-			{
-				method: 'POST',
-				body: JSON.stringify({'username': username})
-			});
-		const msg = await resp.text(
-		alert(msg)
-		)
-	}
-   return (
+  return (
     <>
-      <h4>Welcome to Sales Savvy!</h4>
-	  <form onSubmit={handleSubmit}>
-		<label>Enter username: </label>
-		<input type = "text" name = "username" value = {usrname} 
-		onChange={handleChange}></input>
-		<button type="submit">SEND</button>
-	  </form>
+      <Sign_up />
+	  <hr></hr>
+	  <Sign_in />
     </>
   )
 }
 
 export default App
+
+// import { useState } from "react";
+
+// function App() {
+
+// 	const [username, setUsername] = useState('')
+	
+// 	function handleChange(e){
+// 		setUsername(e.target.value)
+// 	}
+
+// 	async function handleSubmit(e) {
+// 		e.preventDefault() //for avoiding automatic submission
+// 		const resp = await fetch('http://localhost:8080/data',
+// 			{
+// 				method: 'POST',
+// 				body: JSON.stringify({'username': username})
+// 			});
+// 		const msg = await resp.text(
+// 		alert(msg)
+// 		)
+// 	}
+//    return (
+//     <>
+//       <h4>Welcome to Sales Savvy!</h4>
+// 	  <form onSubmit={handleSubmit}>
+// 		<label>Enter username: </label>
+// 		<input type = "text" name = "username" value = {usrname} 
+// 		onChange={handleChange}></input>
+// 		<button type="submit">SEND</button>
+// 	  </form>
+//     </>
+//   )
+// }
+
+// export default App
